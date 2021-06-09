ea10a36c4d5e9c6a03a2e1f8498333fbbc63ab7c
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-06-09 20:41:38 +0100

    style individual tasks

--- src/components/Tasks.vue
+++ src/components/Tasks.vue
@@ -1,16 +1,22 @@
 <template>
-    <div v-for="task in tasks"> 
-        <h3>{{task.text}}</h3>
+    <div :key="task.id" v-for="task in tasks"> 
+        <Task :task="task"/>
  
 
     </div>
 </template>
 
 <script>
+    import Task from './Task'
+
+
     export default {
         name: "Tasks",
         props: {
             tasks: Array
+        },
+        components: {
+            Task
         }
     }
 
