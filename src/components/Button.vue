fbb225ecf252fb2c65cd02aff0e100d0b5bfd09d
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-06-09 17:12:45 +0100

    add task button and style

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+    <button :style="{background:color}">{{text}}</button>
+</template>
+
+<script>
+export default {
+    name: "Button",
+    props: {
+        text: String,
+        color: String
+    }
+}
+</script>
+
+<style scoped>
+button{
+    padding:5px;
+    border-radius: 5px;
+}
+</style>
