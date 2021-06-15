00eeee6898381bbbd0f7cb5f0f2843a62d27f980
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-06-15 17:02:09 +0100

    revert

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,37 +1,31 @@
 <template>
-  <div class="container">
-    <Header @toggle-form= "toggleForm" title="Task Tracker" :showAddTask = "showAddTask" />
-    <div v-show="showAddTask">
-    <AddTask @add-task = "addTask"/>
-    </div>
+    <AddTask v-show="showAddTask" @add-task = "addTask"/>
+  
     <Tasks @toggle-reminder= "toggleReminder" @delete-task= "deleteTask" :tasks="tasks"/>
     
-  </div>
 </template>
 
 <script>
-import Header from './components/Header'
-import Tasks from './components/Tasks'
-import AddTask from './components/AddTask'
+import Tasks from '../components/Tasks'
+import AddTask from '../components/AddTask'
 
-export default {
-  name: 'App',
-  components: {
-    Header,
-    Tasks,
-    AddTask
+export default{
+     name: 'Home',
+     props: {
+         showAddTask: Boolean
+     },
+     components: {
+         Tasks,
+         AddTask
+     },
+     data(){
+         return {
+              tasks: []
+         }
 
-  },
-
-  data(){
-    return {
-      tasks: [],
-      showAddTask: false 
-    }
-  },
+     },
    
     methods:{
-
     async addTask(task){
       console.log(task)
       const res = await fetch('http://localhost:3000/tasks/', {
@@ -62,42 +56,18 @@ export default {
 
     },
 
-    toggleForm(){
-      this.showAddTask = !this.showAddTask
-    }
 
-
-
-  },
 
 
     async created() {
       const res = await fetch ('http://localhost:3000/tasks/')
       const data = await res.json()
       console.log(data)
-      this.tasks = data.tasks
+      this.tasks = data.tasks 
 
-  },
+    }
+    }
+
 }
+ 
 </script>
-
-<style>
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-body {
-  font-family: sans-serif;
-}
-.container {
-  max-width: 500px;
-  margin: 30px auto;
-  overflow: auto;
-  min-height: 300px;
-  border: 1px solid steelblue;
-  padding: 30px;
-  border-radius: 5px;
-}
-
-</style>
