dfc24817bc3617e582e00b53f6b64705820e9a21
Pearl-web <pearlhamilton@hotmail.co.uk>
2021-06-15 17:53:22 +0100

    hide form toggle button on about page

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
         <h1>{{title}}</h1>
-        <Button @btn-click = "$emit('toggle-form')" :text="showAddTask ? 'Close' : 'Add Task' " :color="showAddTask ? 'red' : 'green' "/>
+        <Button v-show="homePage" @btn-click = "$emit('toggle-form')" :text="showAddTask ? 'Close' : 'Add Task' " :color="showAddTask ? 'red' : 'green' "/>
     </header>
 </template>
 
@@ -15,6 +15,18 @@
         },
         components: {
             Button
+        },
+        computed: {
+            homePage(){
+                if(this.$route.path === '/') {
+                    return true
+                }
+                else {
+                    return false
+                }
+        
+
+            }
         }
     }
 
